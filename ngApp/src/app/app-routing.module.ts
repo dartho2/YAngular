bc516c827e56f95d674e6b4386b05b7573950f34
Jan Nowak <jannowak@example.com>
2018-08-13 11:02:31 +0200

    ngAPP

---
+++ ngApp/src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SpecialEventComponent } from './special-event/special-event.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/events',
+    pathMatch: 'full'
+  },
+  {
+    path: 'events',
+    component: EventsComponent
+  },
+  {
+    path: 'special',
+    component: SpecialEventComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
